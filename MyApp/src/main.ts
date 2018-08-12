d28aafea1a268cf4aa87e34b02bb7bf79272280f
Demis Bellot <demis.bellot@gmail.com>
2018-08-12 14:47:45 -0400

    Move Router to router.ts

--- MyApp/src/main.ts
+++ MyApp/src/main.ts
@@ -6,26 +6,10 @@ import 'es6-shim';
 
 import Vue from 'vue';
 import './plugins/vuetify';
-import Router from 'vue-router';
 import App from './App.vue';
-import Home from './home/Home.vue';
-import View1 from './view1/View1.vue';
-import View2 from './view2/View2.vue';
 
-const routes = [
-  { path: '/', component: Home, props: { name: 'Vue' } },
-  { path: '/view1', component: View1 },
-  { path: '/view2', component: View2 },
-  { path: '*', redirect: '/' },
-];
+import router from './router';
 
-const router = new Router ({
-    mode: 'history',
-    linkActiveClass: 'active',
-    routes,
-});
-
-Vue.use(Router);
 const app = new Vue({
     el: '#app',
     render: (h) => h(App),
